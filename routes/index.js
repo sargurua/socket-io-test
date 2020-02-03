39bb97d9b99478563e215c02d7303863692aa762
Avinash Sarguru <avinashsarguru@gmail.com>
2020-02-03 11:24:01 -0600

    added socket.io open

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,12 @@ router.get("/", (req, res) => {
   res.send({ response: "I am alive" }).status(200);
 });
 
+io.on("connection", socket => {
+    console.log("New client connected"), setInterval(
+      () => getApiAndEmit(socket),
+      10000
+    );
+    socket.on("disconnect", () => console.log("Client disconnected"));
+});
+
 module.exports = router;
