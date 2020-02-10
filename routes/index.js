857308d83ce1b0e26d4c36dc5430fefa937f02b4
Avinash Sarguru <avinashsarguru@gmail.com>
2020-02-10 09:43:56 -0600

    clear the interval upon subsequent connections

--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,17 @@ router.get("/", (req, res) => {
   res.send({ response: "I am alive" }).status(200);
 });
 
+let interval;
+
 io.on("connection", socket => {
-    console.log("New client connected"), setInterval(
-      () => getApiAndEmit(socket),
-      10000
-    );
-    socket.on("disconnect", () => console.log("Client disconnected"));
+  console.log("New client connected");
+  if (interval) {
+    clearInterval(interval);
+  }
+  interval = setInterval(() => getApiAndEmit(socket), 10000);
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+  });
 });
 
 module.exports = router;
